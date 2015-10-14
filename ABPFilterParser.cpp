e5e7a5126fdaa5622c18b6b604442491c3de4af5
Brian R. Bondy <netzen@gmail.com>
2015-10-14 14:18:54 -0400

    Add code to post host anchored rules

--- ABPFilterParser.cpp
+++ ABPFilterParser.cpp
@@ -1,5 +1,6 @@
 #include "ABPFilterParser.h"
 #include <string.h>
+#include <iostream>
 
 const char *separatorCharacters = ":?/=^";
 const int maxLineLength = 2048;
@@ -7,6 +8,8 @@ const int maxLineLength = 2048;
 enum FilterParseState {
   FPStart,
   FPPastWhitespace,
+  FPOneBar,
+  FPContent,
 };
 
 // Not currently multithreaded safe due to the static buffer named 'data'
@@ -14,15 +17,27 @@ void parseFilter(const char *input, Filter &f) {
   FilterParseState parseState = FPStart;
   const char *p = input;
   static char data[1024];
+  memset(data, 0, sizeof data);
   int i = 0;
   while (*p != '\0') {
-
     // Check for the filter being too long
-    if (i >= maxLineLength - 1) {
+    if ((p - input) >= maxLineLength - 1) {
       return;
     }
 
     switch (*p) {
+      case '|':
+        if (parseState == FPStart || parseState == FPPastWhitespace) {
+          parseState = FPOneBar;
+          p++;
+          continue;
+        } else if (parseState == FPOneBar) {
+          f.filterType = hostAnchored;
+          parseState = FPContent;
+          p++;
+          continue;
+        }
+        break;
       case '!':
       case '[':
         if (parseState == FPStart || parseState == FPPastWhitespace) {
@@ -50,5 +65,5 @@ void parseFilter(const char *input, Filter &f) {
 
   data[i] = '\0';
   f.data = new char[i];
-  memcpy(f.data, data, i);
+  memcpy(f.data, data, i + 1);
 }
--- test/parser-test.cpp
+++ test/parser-test.cpp
@@ -11,15 +11,26 @@
 
 using namespace std;
 
-bool testFilter( const char *rawFilter, const char *expectedData, set<string> &&blocked, set<string> &&notBlocked) {
+bool testFilter( const char *rawFilter, FilterType expectedFilterType, const char *expectedData, set<string> &&blocked, set<string> &&notBlocked) {
   Filter filter;
   parseFilter(rawFilter, filter);
-  return !strcmp(filter.data, expectedData);
+
+  if (filter.filterType != expectedFilterType) {
+    cout << "Actual filter type: " << filter.filterType << endl << "Expected: " << expectedFilterType << endl;
+    return false;
+  }
+
+  if (strcmp(filter.data, expectedData)) {
+    cout << "Actual filter data: " << filter.data << endl << "Expected: " << expectedData << endl;
+    return false;
+  }
+  return true;
 }
 
 TEST(x, y)
 {
   CHECK(testFilter("/banner/*/img",
+    noFilterType,
     "/banner/*/img",
     {
       "http://example.com/banner/foo/img",
@@ -32,4 +43,225 @@ TEST(x, y)
       "http://example.com/img/banner/",
     }
   ));
+
+  CHECK(testFilter("/banner/*/img^",
+    noFilterType,
+    "/banner/*/img^",
+    {
+      "http://example.com/banner/foo/img",
+      "http://example.com/banner/foo/bar/img?param",
+      "http://example.com/banner//img/foo",
+    }, {
+      "http://example.com/banner/img",
+      "http://example.com/banner/foo/imgraph",
+      "http://example.com/banner/foo/img.gif",
+    }
+  ));
+
+  CHECK(testFilter("||ads.example.com^",
+    hostAnchored,
+    "ads.example.com^",
+    {
+      "http://ads.example.com/foo.gif",
+      "http://server1.ads.example.com/foo.gif",
+      "https://ads.example.com:8000/",
+    }, {
+      "http://ads.example.com.ua/foo.gif",
+      "http://example.com/redirect/http://ads.example.com/",
+    }
+  ));
+
+#if 0
+  ["|http://example.com/|", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: true,
+    rightAnchored: true,
+    options: {},
+    data: "http://example.com/",
+    blocked: [
+      "http://example.com/"
+    ],
+    notBlocked: [
+      "http://example.com/foo.gif",
+      "http://example.info/redirect/http://example.com/",
+    ],
+  }],
+  ["swf|", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: true,
+    options: {},
+    data: "swf",
+    blocked: [
+      "http://example.com/annoyingflash.swf",
+    ],
+    notBlocked: [
+      "http://example.com/swf/index.html"
+    ],
+  }],
+  ["|http://baddomain.example/", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: true,
+    rightAnchored: undefined,
+    options: {},
+    data: "http://baddomain.example/",
+    blocked: [
+     "http://baddomain.example/banner.gif",
+    ],
+    notBlocked: [
+      "http://gooddomain.example/analyze?http://baddomain.example",
+    ],
+  }],
+  ["||example.com/banner.gif", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: true,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "example.com/banner.gif",
+    blocked: [
+      "http://example.com/banner.gif",
+      "https://example.com/banner.gif",
+      "http://www.example.com/banner.gif",
+    ],
+    notBlocked: [
+      "http://badexample.com/banner.gif",
+      "http://gooddomain.example/analyze?http://example.com/banner.gif",
+      "http://example.com.au/banner.gif",
+      "http://example.com/banner2.gif",
+    ],
+  }],
+  ["http://example.com^", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "http://example.com^",
+    blocked: [
+      "http://example.com/",
+      "http://example.com:8000/ ",
+    ],
+    notBlocked: [],
+  }],
+  ["^example.com^", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "^example.com^",
+    blocked: [
+      "http://example.com:8000/foo.bar?a=12&b=%D1%82%D0%B5%D1%81%D1%82",
+    ],
+    notBlocked: [],
+  }],
+  ["^%D1%82%D0%B5%D1%81%D1%82^", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "^%D1%82%D0%B5%D1%81%D1%82^",
+    blocked: [
+      "http://example.com:8000/foo.bar?a=12&b=%D1%82%D0%B5%D1%81%D1%82",
+    ],
+    notBlocked: [
+      "http://example.com:8000/foo.bar?a=12&b%D1%82%D0%B5%D1%81%D1%823",
+    ],
+  }],
+  ["^foo.bar^", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "^foo.bar^",
+    blocked: [
+      "http://example.com:8000/foo.bar?a=12&b=%D1%82%D0%B5%D1%81%D1%82"
+    ],
+    notBlocked: [
+    ],
+  }],
+  ["/banner\\d+/", {
+    isRegex: true,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: undefined,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "banner\\d+",
+    blocked: [
+      "banner123",
+      "testbanner1",
+    ],
+    notBlocked: [
+      "banners",
+      "banners123",
+    ],
+  }],
+  ["||static.tumblr.com/dhqhfum/WgAn39721/cfh_header_banner_v2.jpg", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: true,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: {},
+    data: "static.tumblr.com/dhqhfum/WgAn39721/cfh_header_banner_v2.jpg",
+    blocked: [
+      "http://static.tumblr.com/dhqhfum/WgAn39721/cfh_header_banner_v2.jpg",
+    ],
+    notBlocked: [
+    ],
+  }],
+  ["||googlesyndication.com/safeframe/$third-party", {
+    isRegex: false,
+    isException: false,
+    elementHiding: undefined,
+    elementHidingException: undefined,
+    hostAnchored: true,
+    leftAnchored: undefined,
+    rightAnchored: undefined,
+    options: { "binaryOptions": ["third-party"] },
+    data: "googlesyndication.com/safeframe/",
+    blocked: [
+      "http://tpc.googlesyndication.com/safeframe/1-0-2/html/container.html#xpc=sf-gdn-exp-2&p=http%3A//slashdot.org;",
+    ],
+    notBlocked: [
+    ],
+  }],
+#endif
+
+
 }
