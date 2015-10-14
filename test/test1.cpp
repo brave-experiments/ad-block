f9c01e74b4b9cc0e3b2615fe5f402141f6355622
Brian R. Bondy <netzen@gmail.com>
2015-10-13 22:22:58 -0400

    Add test data and code for reading in a file for tests

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,11 +1,31 @@
 #include "CppUnitLite/TestHarness.h"
 #include "ABPFilterParser.h"
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <cerrno>
 #include "test.h"
+#include <iostream>
+
+using namespace std;
+
+string getFileContents(const char *filename)
+{
+  ifstream in(filename, ios::in);
+  if (in) {
+    ostringstream contents;
+    contents << in.rdbuf();
+    in.close();
+    return(contents.str());
+  }
+  throw(errno);
+}
 
 TEST(x, y)
 {
-  std::string s1("Hello"), s2("Hello"), s3("world");
+  cout << getFileContents("./test/data/easylist.txt") << endl;
+
+  string s1("Hello"), s2("Hello"), s3("world");
   CHECK_EQUAL(s1, s2);
   CHECK_EQUAL(s2, s1);
   CHECK(s1 != s3);
