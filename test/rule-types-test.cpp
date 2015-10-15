f650504a2f6b8f5fe15a68c81ee72d35572f37f6
Brian R. Bondy <netzen@gmail.com>
2015-10-14 21:02:32 -0400

    Add tests for comment rule types

--- test/rule-types-test.cpp
+++ test/rule-types-test.cpp
@@ -11,8 +11,42 @@
 
 using namespace std;
 
-TEST(ruleTypes, test)
-{
+bool testComment( const char *rawFilter) {
+  Filter filter;
+  parseFilter(rawFilter, filter);
+  return filter.filterType == comment;
+}
+
+TEST(ruleTypes, commentRules) {
+  set<string> commentRules {
+    "[Adblock Plus 2.0]",
+    "! Checksum: nVIXktYXKU6M+cu+Txkhuw",
+    "!/cb.php?sub$script,third-party",
+    "!@@/cb.php?sub",
+    "!###ADSLOT_SKYSCRAPER",
+    "! *** easylist:easylist/easylist_whitelist_general_hide.txt ***",
+    "   !###ADSLOT_SKYSCRAPER",
+  };
+
+  std::for_each(commentRules.begin(), commentRules.end(), [this, &result_](string const &s) {
+    CHECK(testComment(s.c_str()));
+  }
+);
+
+TEST(ruleTypes, elemHidingRules) {
   CHECK(true);
 }
 
+/*
+let elementHidingRules = new Set([
+  "   ###ADSLOT_SKYSCRAPER",
+  "###ADSLOT_SKYSCRAPER",
+  "@@###ADSLOT_SKYSCRAPER",
+  "##.adsBox",
+  "eee.se#@##adspace_top",
+  "domain1.com,domain2.com#@##adwrapper",
+  "edgesuitedomain.net#@##ad-unit",
+  "mydomain.com#@#.ad-unit",
+  "##a[href^=\"http://affiliate.sometracker.com/\"]",
+]);
+*/
