448ac06df7f33b960afbeb0196f8d76f7e22a1b5
Brian R. Bondy <netzen@gmail.com>
2016-10-25 10:58:32 -0400

    Use default lists when UUID isn't specified

--- scripts/discoverBlockingRules.js
+++ scripts/discoverBlockingRules.js
@@ -4,9 +4,10 @@
 
 // Example invocations:
 // node scripts/discoverBlockingRules.js  --uuid 03F91310-9244-40FA-BCF6-DA31B832F34D --host slashdot.org --location https://s.yimg.jp/images/ds/ult/toppage/rapidjp-1.0.0.js
+// node scripts/discoverBlockingRules.js  --host slashdot.org --location https://s.yimg.jp/images/ds/ult/toppage/rapidjp-1.0.0.js
 
 const commander = require('commander')
-const {makeAdBlockClientFromListUUID} = require('../lib/util')
+const {makeAdBlockClientFromListUUID, makeAdBlockClientFromFilePath} = require('../lib/util')
 const {FilterOptions} = require('..')
 
 commander
@@ -15,10 +16,19 @@ commander
   .option('-l, --location [location]', 'URL to use for the check')
   .parse(process.argv)
 
-let p = Promise.reject('Usage: node discoveryBlockingRules.js --location <location> --host <host> --uuid <uuid>')
+let p = Promise.reject('Usage: node discoveryBlockingRules.js --location <location> --host <host> [--uuid <uuid>]')
 
-if (commander.uuid && commander.host && commander.location) {
-  p = makeAdBlockClientFromListUUID(commander.uuid)
+if (commander.host && commander.location) {
+  if (commander.uuid) {
+    p = makeAdBlockClientFromListUUID(commander.uuid)
+  } else {
+    const defaultAdblockLists = [
+      './test/data/easylist.txt',
+      './test/data/ublock-unbreak.txt',
+      './test/data/brave-unbreak.txt'
+    ]
+    p = makeAdBlockClientFromFilePath(defaultAdblockLists)
+  }
 }
 
 p.then((adBlockClient) => {
