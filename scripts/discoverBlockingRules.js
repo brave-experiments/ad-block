ddf0f384691e164352bf107b1faa01cc30953a90
Brian R. Bondy <netzen@gmail.com>
2016-10-25 10:46:23 -0400

    Make lookup script take in args

--- scripts/discoverBlockingRules.js
+++ scripts/discoverBlockingRules.js
@@ -2,14 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const url = 'https://s.yimg.jp/images/ds/ult/toppage/rapidjp-1.0.0.js'
-const uuid = '03F91310-9244-40FA-BCF6-DA31B832F34D'
+// Example invocations:
+// node scripts/discoverBlockingRules.js  --uuid 03F91310-9244-40FA-BCF6-DA31B832F34D --host slashdot.org --location https://s.yimg.jp/images/ds/ult/toppage/rapidjp-1.0.0.js
+
+const commander = require('commander')
 const {makeAdBlockClientFromListUUID} = require('../lib/util')
 const {FilterOptions} = require('..')
 
-makeAdBlockClientFromListUUID(uuid).then((adBlockClient) => {
+commander
+  .option('-u, --uuid [uuid]', 'UUID of the list to use')
+  .option('-h, --host [host]', 'host of the page that is being loaded')
+  .option('-l, --location [location]', 'URL to use for the check')
+  .parse(process.argv)
+
+let p = Promise.reject('Usage: node discoveryBlockingRules.js --location <location> --host <host> --uuid <uuid>')
+
+if (commander.uuid && commander.host && commander.location) {
+  p = makeAdBlockClientFromListUUID(commander.uuid)
+}
+
+p.then((adBlockClient) => {
   console.log('Parsing stats:', adBlockClient.getParsingStats())
-  console.log('Matches: ', adBlockClient.matches(url, FilterOptions.noFilterOption, 'slashdot.org'))
+  console.log('Matches: ', adBlockClient.matches(commander.location, FilterOptions.noFilterOption, commander.host))
 }).catch((e) => {
   console.log('Error:', e)
 })
