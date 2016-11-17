09c6d20401e077e0d6c89008458b8431a82b8c24
Brian R. Bondy <netzen@gmail.com>
2016-11-17 15:52:52 -0500

    Add ability to check a list of sites to check.js

--- scripts/check.js
+++ scripts/check.js
@@ -6,9 +6,10 @@
 // node scripts/check.js  --uuid 03F91310-9244-40FA-BCF6-DA31B832F34D --host slashdot.org --location https://s.yimg.jp/images/ds/ult/toppage/rapidjp-1.0.0.js
 // node scripts/check.js  --host www.cnet.com --location https://s0.2mdn.net/instream/html5/ima3.js
 // node scripts/check.js --dat ./out/SafeBrowsingData.dat --host excellentmovies.net --location https://excellentmovies.net
+// node scripts/check.js  --host www.cnet.com --list ./test/data/sitelist.txt
 
 const commander = require('commander')
-const {makeAdBlockClientFromListUUID, makeAdBlockClientFromDATFile, makeAdBlockClientFromFilePath, makeAdBlockClientFromString} = require('../lib/util')
+const {makeAdBlockClientFromListUUID, makeAdBlockClientFromDATFile, makeAdBlockClientFromFilePath, makeAdBlockClientFromString, readSiteList} = require('../lib/util')
 const {FilterOptions} = require('..')
 
 commander
@@ -18,11 +19,12 @@ commander
   .option('-h, --host [host]', 'host of the page that is being loaded')
   .option('-l, --location [location]', 'URL to use for the check')
   .option('-o, --output [output]', 'Optionally saves a DAT file')
+  .option('-L --list [list]', 'Filename for list of sites to check')
   .parse(process.argv)
 
 let p = Promise.reject('Usage: node check.js --location <location> --host <host> [--uuid <uuid>]')
 
-if (commander.host && commander.location) {
+if (commander.host && (commander.location || commander.list)) {
   p.catch(() => {})
   if (commander.uuid) {
     p = makeAdBlockClientFromListUUID(commander.uuid)
@@ -42,7 +44,25 @@ if (commander.host && commander.location) {
 
 p.then((adBlockClient) => {
   console.log('Parsing stats:', adBlockClient.getParsingStats())
-  console.log('Matches: ', adBlockClient.matches(commander.location, FilterOptions.noFilterOption, commander.host))
+  if (commander.location) {
+    console.log('Matches: ', adBlockClient.matches(commander.location, FilterOptions.noFilterOption, commander.host))
+  } else {
+    const siteList = readSiteList(commander.list)
+    let matchCount = 0
+    let skipCount = 0
+    console.time('check')
+    siteList.forEach((site) => {
+      if (adBlockClient.matches(site, FilterOptions.noFilterOption, commander.host)) {
+        matchCount++
+      } else {
+        skipCount++
+      }
+    })
+    console.timeEnd('check')
+    console.log(adBlockClient.getMatchingStats())
+    console.log('Matching:', matchCount)
+    console.log('Skipped:', skipCount)
+  }
   if (commander.output) {
     require('fs').writeFileSync(commander.output, adBlockClient.serialize())
   }
