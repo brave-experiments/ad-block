70e5dcdc8053ef968fb18470c2da4c15b59c1a0f
Brian R. Bondy <netzen@gmail.com>
2017-10-23 22:22:54 -0400

    Fix missing algorithms include for linux
    Auditors: @SergeyZhukovsky

--- ad_block_client_wrap.cc
+++ ad_block_client_wrap.cc
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <node_buffer.h>
 #include "./ad_block_client_wrap.h"
+#include <node_buffer.h>
+#include <algorithm>
 #include "./data_file_version.h"
 #include "./filter_list.h"
 #include "./lists/regions.h"
