b74f508be52931604199195905d3113bf450c851
bridiver <github@brianjohnson.cc>
2017-10-28 14:15:24 -0700

    build internal module auditors @bbondy

--- addon.cc
+++ addon.cc
@@ -15,6 +15,15 @@ void InitAll(Local<Object> exports) {
   ad_block_client_wrap::AdBlockClientWrap::Init(exports);
 }
 
+void InitAll(v8::Local<v8::Object> exports, v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context, void* priv) {
+  InitAll(exports);
+}
+
 }  // namespace
 
+#ifdef AD_BLOCK_INTERNAL_MODULE
+NODE_MODULE_CONTEXT_AWARE_BUILTIN(ad_block, InitAll)
+#else
 NODE_MODULE(ad_block, InitAll)
+#endif
