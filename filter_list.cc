11a4617c8bf3f52668e76cd4d165629294d9cc18
Emerick Rogul <erogul@brave.com>
2018-05-31 15:50:40 -0400

    Add component id's to ad block filter lists This ensures that the component id's live in a single location and are

--- filter_list.cc
+++ filter_list.cc
@@ -13,12 +13,14 @@ FilterList::FilterList(const std::string& uuid,
                        const std::string& title,
                        const std::vector<std::string>& langs,
                        const std::string& support_url,
+                       const std::string& component_id,
                        const std::string& base64_public_key)
     : uuid(uuid),
       url(url),
       title(title),
       langs(langs),
       support_url(support_url),
+      component_id(component_id),
       base64_public_key(base64_public_key) {}
 
 FilterList::FilterList(const FilterList& other) = default;
